9565e0130c1997c30592aff0f23884d61914bacf
tomasi-metaversal <thomas@metaversal.gg>
2025-07-06 21:22:52 +1000

    update link preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,37 @@ import { Toaster } from "@/components/ui/sonner";
 import { manrope, bespokeSans } from "./fonts";
 
 export const metadata: Metadata = {
-  title: "Midjournal",
-  description: "Your journaling companion",
+  metadataBase: new URL("https://midjournal.me"),
+  title: "Midjournal - SEE HOW YOU FEEL🪞",
+  description:
+    "Go deep, understand yourself better, level up how you engage with the world.",
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    title: "Midjournal - SEE HOW YOU FEEL🪞",
+    description:
+      "Go deep, understand yourself better, level up how you engage with the world.",
+    url: "/",
+    siteName: "Midjournal",
+    images: [
+      {
+        url: "/hero-image-desktop.png",
+        width: 1200,
+        height: 630,
+        alt: "A preview image for Midjournal",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Midjournal - SEE HOW YOU FEEL🪞",
+    description:
+      "Go deep, understand yourself better, level up how you engage with the world.",
+    images: ["/hero-image-desktop.png"],
+  },
 };
 
 export default function RootLayout({
